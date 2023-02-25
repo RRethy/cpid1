65f05254a1db4f6386edba93a97152301e6909cc
Adam P. Regasz-Rethy <adam.regaszrethy@gmail.com>
2023-02-25 15:13:23 -0500

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::process::Command;
 
 fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = std::env::args().collect::<Vec<_>>();
-    println!("command: {:?}", args);
+
+    // handle_signals().await;
+
     Command::new(&args[1])
         .args(&args[2..])
         .spawn()
         .expect("failed to execute process")
         .wait()
         .expect("failed to wait on process");
+    Ok(())
 }
